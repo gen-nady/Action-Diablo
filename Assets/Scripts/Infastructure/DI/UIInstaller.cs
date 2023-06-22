b814d3c71f45c181b8eb50efb5b955ed36625000
Gennadiy Shushkevich <gennadiy13122@gmail.com>
2023-06-22 21:31:07 +0300

    fix-bugs

--- Assets/Scripts/Infastructure/DI/UIInstaller.cs
+++ Assets/Scripts/Infastructure/DI/UIInstaller.cs
@@ -7,18 +7,14 @@ namespace Infastructure
     public class UIInstaller : MonoInstaller
     {
         [SerializeField] private PlayerQuestUI _playerQuestUI;
-        [SerializeField] private QuestGiverUI _questGiverUI; 
-      //  [SerializeField] private WorldInfoUI _worldInfoUI;
+        [SerializeField] private QuestGiverUI _questGiverUI;
         [SerializeField] private TalkQuestUI _talkQuestUI;
-       // [SerializeField] private HeroAttackUI _heroAttackUI;
         
         public override void InstallBindings()
         {
             Container.Bind<PlayerQuestUI>().FromInstance(_playerQuestUI).AsSingle();
             Container.Bind<QuestGiverUI>().FromInstance(_questGiverUI).AsSingle();  
-        //    Container.Bind<WorldInfoUI>().FromInstance(_worldInfoUI).AsSingle();
             Container.Bind<TalkQuestUI>().FromInstance(_talkQuestUI).AsSingle();
-         //   Container.Bind<HeroAttackUI>().FromInstance(_heroAttackUI).AsSingle();
         }
     }
 }
