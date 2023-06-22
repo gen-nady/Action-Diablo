ca5b28bbd16bd87ec004b8673735c6132adadc9a
Gennadiy Shushkevich <gennadiy13122@gmail.com>
2023-06-22 22:12:42 +0300

    add-textures

--- Assets/Scripts/MainPlayer/PlayerMovement.cs
+++ Assets/Scripts/MainPlayer/PlayerMovement.cs
@@ -4,15 +4,18 @@ namespace MainPlayer
 {
     public class PlayerMovement : MonoBehaviour
     {
+        [Header("Moving")]
         [SerializeField] private FloatingJoystick _joystick;
         [SerializeField] private CharacterController _characterController;
         [SerializeField] private Transform _transform;
-        [SerializeField] private Animator _animator;
-        private readonly int _speedPlayer = Animator.StringToHash("Speed");
         [SerializeField] private float _speed;
+        [Header("Moving animation")]
+        [SerializeField] private Animator _animator;
+        private readonly int _speedPlayerHash = Animator.StringToHash("Speed");
         
         private InputController _inputController;
-        
+
+        #region MONO
         private void Awake()
         {
             _inputController = new InputController(_joystick);
@@ -20,13 +23,24 @@ namespace MainPlayer
 
         private void Update()
         {
+            Move();
+            AnimationMove();
+        }
+        #endregion
+        
+        private void Move()
+        {
             if (_inputController.GetAxis() != Vector3.zero)
             {
                 var angle = Mathf.Atan2(_inputController.GetAxis().x, _inputController.GetAxis().z) * Mathf.Rad2Deg;
                 _transform.rotation = Quaternion.Euler(0, angle, 0);
                 _characterController.Move(_inputController.GetAxis() * _speed * Time.deltaTime);
             }
-            _animator.SetFloat(_speedPlayer, Mathf.Max(Mathf.Abs(_inputController.GetAxis().x),Mathf.Abs(_inputController.GetAxis().z)));
+        }
+
+        private void AnimationMove()
+        {
+            _animator.SetFloat(_speedPlayerHash, Mathf.Max(Mathf.Abs(_inputController.GetAxis().x),Mathf.Abs(_inputController.GetAxis().z)));
         }
     }
 }
